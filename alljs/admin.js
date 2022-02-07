6bc186974840ada1323f218411ea3dd53ec624b3
xiye <xiye@fortop.fortop.com.tw>
2022-02-07 10:44:26 +0800

    移除console.log

--- alljs/admin.js
+++ alljs/admin.js
@@ -12,15 +12,12 @@ const app = Vue.createApp({
     },
 		getProducts() {
 			const url = 'https://vue3-course-api.hexschool.io/v2'
-			console.log(this.user)
 			axios.get(`${url}/api/ciye-project/admin/products`)
 			.then((response) => {
-				console.log(response, '六角產品')
 				this.products = response.data.products
 				this.loading = false
 			})
-			.catch((err) => {
-				console.log(err)
+			.catch(() => {
 			})
 		},
 		signinCheck() {
@@ -29,10 +26,10 @@ const app = Vue.createApp({
 			this.loading = true
 			const url = 'https://vue3-course-api.hexschool.io/v2'
 			axios.post(`${url}/api/user/check`)
-			.then((response) => {
+			.then(() => {
 				this.getProducts()
 			})
-			.catch((err) => {
+			.catch(() => {
 				window.location = 'index.html'
 			})
 		}
--- alljs/products.js
+++ alljs/products.js
@@ -19,10 +19,8 @@ const app = Vue.createApp({
 		},
 		deleteImg() {
 			this.product.imagesUrl.pop()
-			console.log(this.product)
 		},
     showProductModel(product) {
-			console.log(product)
 			if (product.title) {
 				this.modalTitle = '編輯產品'
 				this.isEditing = true
@@ -46,15 +44,12 @@ const app = Vue.createApp({
 		},
 		getProducts() {
 			const url = 'https://vue3-course-api.hexschool.io/v2'
-			console.log(this.user)
 			axios.get(`${url}/api/ciye-project/admin/products`)
 			.then((response) => {
-				console.log(response, 'asd')
 				this.products = response.data.products
 				this.loading = false
 			})
-			.catch((err) => {
-				console.log(err)
+			.catch(() => {
 			})
 		},
 		uploadProduct() {
@@ -67,10 +62,8 @@ const app = Vue.createApp({
 			let postProduct = {
 				'data': this.product
 			}
-			console.log(this.product)
 			axios.post(`${url}/api/ciye-project/admin/product`, postProduct)
 			.then((response) => {
-				console.log(response, '新增產品')
 				this.loading = false
 				this.hideModel()
 				this.getProducts()
@@ -79,7 +72,6 @@ const app = Vue.createApp({
 				alert('上傳失敗')
 				this.hideModel()
 				this.loading = false
-				console.log(err)
 			})
 		},
 		editProduct() {
@@ -93,8 +85,7 @@ const app = Vue.createApp({
 				this.hideModel()
 				this.getProducts()
 			})
-			.catch((err) => {
-				console.log(err)
+			.catch(() => {
 			})
 		},
 		deleteProduct() {
@@ -105,8 +96,7 @@ const app = Vue.createApp({
 				this.hideModel()
 				this.getProducts()
 			})
-			.catch((err) => {
-				console.log(err)
+			.catch(() => {
 			})
 		},
 		signinCheck() {
